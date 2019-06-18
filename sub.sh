1a90381e61b63ded765a19146035229f5fbc1a6e
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2019-06-18 00:41:34 -0500

    Create sub.sh
    Bash script to grab subdomain IP addresses from a URL.

---
+++ sub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This will grab subdomains ip addresses from a site
+echo "Enter the site to query: "
+read url
+echo "[+] Grabbing subdomains from $url"
+wget $url
+grep "href=" index.html |cut -d "/" -f 3 |grep "\." |cut -d '"' -f 1 | sort -u > list.txt
+for sub in $(cat list.txt); do host $sub; done |grep "has address" |cut -d " " -f 4 | sort -u > subdomains.txt
+echo [+] The task has been completed.
