0d87e30173498c35a729ab40110d9f8ed452155e
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2020-04-14 22:51:52 -0500

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -6,4 +6,4 @@ echo "[+] Grabbing subdomains from $url"
 wget $url
 grep "href=" index.html |cut -d "/" -f 3 |grep "\." |cut -d '"' -f 1 | sort -u > list.txt
 for sub in $(cat list.txt); do host $sub; done |grep "has address" |cut -d " " -f 4 | sort -u > subdomains.txt
-echo [+] The task has been completed.
+echo "[+] The task has been completed."
